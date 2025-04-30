67983b3267a3d85b328f16a7a17eb758334864ea
raka-arrayan <rakaarrayan27@gmail.com>
2025-04-30 19:37:44 +0700

    tambah domain

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
     const cors = require("cors");
     app.use(cors({
-        origin: ['http://localhost:5174','https://os.netlabdte.com', 'http://localhost:3000'],
+        origin: ['http://localhost:5174','https://os.netlabdte.com', 'http://localhost:3000','https://tp-rakaarrayan.vercel.app/','https://tp-rakaarrayan.vercel.app'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
