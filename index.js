0cc0a89ca7f1cbca7ba980b38225dcb2e31ee284
raka-arrayan <rakaarrayan27@gmail.com>
2025-05-01 14:41:28 +0700

    memperbaiki domain

--- index.js
+++ index.js
@@ -6,11 +6,16 @@
 
     const cors = require("cors");
     app.use(cors({
-        origin: ['http://localhost:5173','http://localhost:5174','https://os.netlabdte.com', 'http://localhost:3000', 'https://tp-rakaarrayan.vercel.app',],
+        origin: [
+            'https://tp-rakaarrayan.vercel.app',  // frontend production
+            'http://localhost:5173',              // frontend local dev (Vite)
+            'http://localhost:5174',              // optional: another dev port
+        ],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: "true",
+        credentials: true,
     }));
+    
 
     app.options('*', cors());
 
